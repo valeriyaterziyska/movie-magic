b179bfffeac093f3310a7f2e1fafff2a13dda79b
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-01-18 16:39:34 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,14 @@
-const movies = [];
+const movies = [
+    {
+        title: 'Jungle Cuise',
+        genre: 'Advunture',
+        director: 'Spilberg',
+        year: '2019',
+        imageUrl: '/img/jungle-cruise.jpeg',
+        rating: '5',
+        description: 'Description movie'
+    }
+];
 
 exports.create = (movieData) => {
     movies.push(movieData);
