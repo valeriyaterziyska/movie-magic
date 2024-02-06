3cc03b2071d1e2a38fb9319312280045a15bdcc8
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-06 16:15:16 +0200

    Attach cast to movie function

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -33,9 +33,17 @@ router.get('/movies/:movieId', async (req, res) => {
 router.get('/movies/:movieId/attach', async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
     const casts = await castService.getAll().lean();
-
     //TODO: remove already added casts
     res.render('movie/attach', { ...movie, casts });
+});
+
+router.post('/movies/:movieId/attach', async (req, res) => {
+    const castId = req.body.cast;
+    const movieId = req.params.movieId;
+
+    await movieService.attach(movieId, castId)
+    
+    res.redirect(`/movies/${movieId}/attach`);
 });
 
 module.exports = router;
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,26 +1,36 @@
 const Movie = require('../models/Movie');
 
-exports.getAll = () =>  Movie.find();
+exports.getAll = () => Movie.find();
 
 //TODO: Filter result in mongoDB
 exports.search = async (title, genre, year) => {
     let newMovies = await Movie.find().lean();
-    
+
     if (title) {
         newMovies = newMovies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
     }
-    
+
     if (genre) {
         newMovies = newMovies.filter(movie => movie.genre.toLowerCase() === genre.toLowerCase());
     }
-    
+
     if (year) {
         newMovies = newMovies.filter(movie => movie.year === year);
     }
-    
+
     return newMovies;
 }
 
 exports.getOne = (movieId) => Movie.findById(movieId);
 
-exports.create = (movieData) =>  Movie.create(movieData);
+exports.create = (movieData) => Movie.create(movieData);
+
+exports.attach = (movieId, castId) => {
+    // const movie = await this.getOne(movieId);
+
+    // //TODO: validate castid if exists
+    // movie.casts.push(castId);
+    // return movie.save();
+
+    Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
+}
