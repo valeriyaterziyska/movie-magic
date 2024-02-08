ee5ffc7f0754f88cd1841eb671b14dcc5115fd2a
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-08 13:01:33 +0200

    Add edit movie page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -43,12 +43,14 @@ router.post('/movies/:movieId/attach', async (req, res) => {
     const movieId = req.params.movieId;
 
     await movieService.attach(movieId, castId)
-    
+
     res.redirect(`/movies/${movieId}/attach`);
 });
 
-router.get('/movies/:movieId/edit', (req, res) => {
-    res.render('movie/edit');
+router.get('/movies/:movieId/edit', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId).lean();
+
+    res.render('movie/edit', { movie });
 });
 
 module.exports = router;
