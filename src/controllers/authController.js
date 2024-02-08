a260cd9ac5c14a1270cae4f58c763790e7206836
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-08 12:40:24 +0200

    Generate token on succesful token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,6 +16,14 @@ router.post('/register', async (req, res) => {
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
-})
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const token = await authService.login(email, password);
+    console.log(token);
+    res.redirect('/');
+});
 
 module.exports = router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,36 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
 
+const SECRET = 'kajshdkahsdkahskdahasda65s4da31sdk';
+
+//TODO: Check if user exists
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+    // Get user from DB
+    const user = await User.findOne({ email });
+
+    // Check if user exists
+    if (!user) {
+        throw new Error('Cannot find email or password');
+    }
+
+    // Check if password is valid
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Cannot find email or password');
+    }
+
+    // Generate jwt token
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    }
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
+    // return token
+    return token;
+
+}; 
