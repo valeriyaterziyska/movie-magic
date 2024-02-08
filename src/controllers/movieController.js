d820925415d3a701f29c1ae91e142e33f235b174
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-08 16:06:55 +0200

    Add dynamic in the nav bar

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -27,7 +27,7 @@ router.post('/create', isAuth, async (req, res) => {
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
-    const isOwner = movie.owner == req.user._id;
+    const isOwner = movie.owner == req.user?._id;
 
     // TODO: This is not perfect, use handlebars helpers
     movie.rating = new Array(Number(movie.rating)).fill(true);
