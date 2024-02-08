ae5bf0e7c0ebeeae0494dd4bd9a747be58f0a18d
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-02-08 16:07:11 +0200

    Add logout

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('../lib/jwt');
 const { SECRET } = require('../config/config');
+const { decode } = require('jsonwebtoken');
 
 exports.auth = async (req, res, next) => {
     const token = req.cookies['auth'];
@@ -11,6 +12,7 @@ exports.auth = async (req, res, next) => {
     try {
         const decodedToken = await jwt.verify(token, SECRET);
         req.user = decodedToken;
+        res.locals.isAuthenticated = true;
 
         next();
     } catch (error) {
